cfb1dc53c21d4fa060dcdaf9763eab89bce01df1
thachhnph04651 <nhom1fpoly@gmail.com>
2017-07-03 10:05:51 +0700

    update

--- Assignment/SanPham.aspx.designer.cs
+++ Assignment/SanPham.aspx.designer.cs
@@ -13,22 +13,13 @@ namespace Assignment {
     public partial class WebForm1 {
         
         /// <summary>
-        /// GridView1 control.
+        /// FormView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
-        
-        /// <summary>
-        /// SqlDataSource2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
+        protected global::System.Web.UI.WebControls.FormView FormView1;
         
         /// <summary>
         /// SqlDataSource1 control.
